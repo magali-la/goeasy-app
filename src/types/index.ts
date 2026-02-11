f4b63197f6c5ae240405f499fc09c6f8a3d8665e
Mag <166319982+magali-la@users.noreply.github.com>
2026-02-11 11:27:06 -0500

    chore(types): add User type and AuthResponse type for backend data

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+// TYPES FOR DATA FROM THE BACKEND
+
+// Provider type - how the user signed up
+export type Providers = 'local' | 'google';
+
+// User Type - user object fields from the backend that will be used in the UI
+export interface User {
+    _id: string;
+    username: string;
+    email: string;
+    provider: Providers;
+    // the timestamp will come back as well for profile
+    createdAt: string;
+    updatedAt: string;    
+}
+
+// Auth Response - the shape of the response the backend sends to the client for login/signup/google oauth routes
+export interface AuthResponse {
+    token: string;
+    user: User;
+}
