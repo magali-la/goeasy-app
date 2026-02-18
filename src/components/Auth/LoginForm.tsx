6240ba032c1ac884d31f563a5bccc3c506c0d463
Mag <166319982+magali-la@users.noreply.github.com>
2026-02-17 23:31:26 -0500

    feat(login): fix issue withe toggle button submitting form, add login page and form, add cursor styling for buttons in index.css

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -4,9 +4,8 @@ import { useState } from "react";
 import { axiosInstance } from "../../services/axios";
 import Button from "../Button";
 
-export default function SignupForm() {
+export default function LoginForm() {
     // controlled inputs
-    const [username, setUsername] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     // set an error state to show validation messages
@@ -19,41 +18,25 @@ export default function SignupForm() {
     const navigate = useNavigate();
 
     // submit form handler
-    async function handleSignup(event: React.SubmitEvent<HTMLFormElement>) {
+    async function handleLogin(event: React.SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
 
         // form validation logic
-        if (!username || !email || !password) {
+        if (!email || !password) {
             setError("Please fill out all required fields.");
             // return it to show the error below the form
             return;
         }
 
-        // email errors - regex from backend
-        const emailRegex = /.+@.+\..+/;
-        if (!emailRegex.test(email)) {
-            setError("Please enter a valid email.");
-            return;
-        }
-
-        // passwrd errrs - regex from backend
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
-        if (!passwordRegex.test(password)) {
-            setError("Password must be at least 8 characters and include at least 1 letter and 1 number.");
-            return;
-        }
-
         // try catch with axios, then call login function from auth provider
         try {
-            // TO DELETE set testInput prior to form creation - check when messing up the password and email
-            const newUser = {
-                username,
+            const returningUser = {
                 email,
                 password
             }
 
             // axios call for the route
-            const res = await axiosInstance.post('/api/users/signup', newUser)
+            const res = await axiosInstance.post('/api/users/login', returningUser)
 
             // call login to store the user
             login(res.data.user);
@@ -67,12 +50,12 @@ export default function SignupForm() {
                 console.log(error.response.status, error.response.data.message);
             } else {
                 // generic error if not
-                console.error("Signup failed", error);
+                console.error("Login failed", error);
             } 
         }
     }
 
-    async function handleGoogleSignup() {
+    async function handleGoogleLogin() {
         try {
             // this will follow the redirect initiated in the OAuth flow - backend will manage redirecting to home
             window.location.href = import.meta.env.VITE_GOOGLE_AUTH_URL;
@@ -82,7 +65,7 @@ export default function SignupForm() {
                 console.log(error.response.status, error.response.data.message);
             } else {
                 // generic error if not
-                console.error("Google Signup failed", error);
+                console.error("Google Login failed", error);
             } 
         } 
     }
@@ -90,27 +73,21 @@ export default function SignupForm() {
     return (
         // remove browser tooltips for form validation
         <form className="rounded-2xl bg-white h-1/2 p-8 flex flex-col gap-4 min-w-1/2 justify-center items-center" aria-label="signup for an account" onSubmit={handleSignup} noValidate>
-            <h2 className="font-dela self-start">Create an Account</h2>
+            <h2 className="font-dela self-start">Login to your Account</h2>
 
             {/* google button */}
-            <button type="button" className="py-2 px-5 cursor-pointer shadow-2xs hover:shadow-md rounded-lg border-dark/15 border-2" onClick={handleGoogleSignup}><i className="bi bi-google mr-2"></i>Sign up with Google</button>
+            <button type="button" className="py-2 px-5 cursor-pointer shadow-2xs hover:shadow-md rounded-lg border-dark/15 border-2" onClick={handleGoogleLogin}><i className="bi bi-google mr-2"></i>Sign in with Google</button>
 
-            <p className="font-medium text-lg">Or signup with email</p>
+            <p className="font-medium text-lg">Or login with email</p>
 
             {/* form elements */}
-            {/* username input */}
-            <label htmlFor="username" className="font-medium text-lg self-start">Username</label>
-            <small className="self-start">Username must be 6-15 characters</small>
-            <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} placeholder="Enter username"/>
-
             {/* email input */}
             <label htmlFor="email" className="font-medium text-lg self-start">Email</label>
             <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter email"/>
 
             {/* password input */}
             <label htmlFor="password" className="font-medium text-lg self-start">Password</label>
-            <small className="self-start">Password must be at least 8 characters and contain 1 number and 1 letter</small>
-            
+    
             {/* wrap in continer for the toggle button */}
             <div className="relative w-full">
                 {/* change the input type if the showPassword is on or not */}
@@ -125,14 +102,14 @@ export default function SignupForm() {
                 </button>
             </div>
             
-            <p>Already have an account? <span className="text-berry"><Link to="/login">Login here.</Link></span></p>
+            <p>Don't have an account? <span className="text-berry"><Link to="/signup">Signup here.</Link></span></p>
 
             {/* show the error above the submit button conditionally */}
             {error && (
                 <p className="text-red-800">{error}</p>
             )}
             {/* submit button */}
-            <Button type="submit" shape="auth" label="Sign Up" className="bg-berry"/>
+            <Button type="submit" shape="auth" label="Login" className="bg-berry"/>
         </form>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
+import LoginForm from "../components/Auth/LoginForm";
+
 export default function Login() {
     return (
-        <>
-            <h1>Login Page</h1>
-        </>
+        <div className="relative min-h-full p-10">
+            <div className="flex flex-col md:flex-row justify-between gap-10">
+                <div className="flex flex-col gap-8 md:gap-20">
+                    <h1 className="font-dela">Your travels await you...</h1>
+                    <img className="rounded-2xl shadow-lg shadow-liteberry" src="https://images.unsplash.com/photo-1758599668652-291575a50dff?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="four friends with backpacks taking a selfie in the woods"/>
+                </div>
+                <LoginForm/>
+            </div>
+        </div>
     )
 }
