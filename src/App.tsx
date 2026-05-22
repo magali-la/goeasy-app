977bcc5362a29c0f77d7e26ae1f2c7c203b228d0
Mag <166319982+magali-la@users.noreply.github.com>
2026-05-22 01:39:07 -0400

    feat(animations): add PageTransition wrapper with conditional enter and exit animations with AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
 import './index.css'
 import Landing from './pages/Landing'
 import Signup from './pages/Signup'
@@ -12,41 +12,43 @@ import Explore from './pages/Explore'
 import ExploreCity from './pages/ExploreCity'
 import Footer from './components/Footer'
 import { useScrollRestoration } from './hooks/useScrollRestoration'
+import { PageTransition } from './layout/PageTransition'
 
 function App() {
 	// scroll poosition restore, cann the hook
 	useScrollRestoration();
+	// need location for animate presence to work
+	const location = useLocation();
 	
 	return (
 		<>
 			<NavBar/>
-			<Routes>
-				{/* Landing page */}
-				<Route index element={<Landing/>}/>
-
-				{/* signup */}
-				<Route path='signup' element={<Signup/>}/>
-
-				{/* login */}
-				<Route path='login' element={<Login/>}/>
-
-				{/* protected routes */}
-				<Route element={<ProtectedRoute/>}>
-					{/* authenticated home */}
-					<Route path='home' element={<Dashboard/>}/>
-
-					{/* trips routes */}
-					<Route path='trips'>
-						<Route index element={<Trips/>} />
-						<Route path=':tripId' element={<TripDetail/>} />
+			<PageTransition>
+				{/* key is needed to override default react router behavior and rebuild the route tree so animate presence can capture the prev page component for the exit animation */}
+				<Routes location={location} key={location.pathname}>
+					{/* Landing page */}
+					<Route index element={<Landing/>}/>
+					{/* signup */}
+					<Route path='signup' element={<Signup/>}/>
+					{/* login */}
+					<Route path='login' element={<Login/>}/>
+					{/* protected routes */}
+					<Route element={<ProtectedRoute/>}>
+						{/* authenticated home */}
+						<Route path='home' element={<Dashboard/>}/>
+						{/* trips routes */}
+						<Route path='trips'>
+							<Route index element={<Trips/>} />
+							<Route path=':tripId' element={<TripDetail/>} />
+						</Route>
+						{/* explore routes */}
+						<Route path='explore'>
+							<Route index element={<Explore/>} />
+							<Route path=':cityId' element={<ExploreCity/>} />
+						</Route>
 					</Route>
-					{/* explore routes */}
-					<Route path='explore'>
-						<Route index element={<Explore/>} />
-						<Route path=':cityId' element={<ExploreCity/>} />
-					</Route>
-				</Route>
-			</Routes>
+				</Routes>
+			</PageTransition>
 			<Footer/>
 		</>
 	)
---
+++ src/layout/PageTransition.tsx
@@ -0,0 +1,32 @@
+import { AnimatePresence, easeIn, easeInOut, easeOut, motion } from "motion/react"
+import { useLocation } from "react-router"
+
+// this is being used as an entire wrapper around the Routes in App.tsx. it needs children, not outlet to correctly access the page component in the route children
+export function PageTransition({ children }: { children: React.ReactNode}) {
+    const location = useLocation()
+
+    // variable if the path is the landing, uses custom exit animation only 
+    const isLanding = location.pathname === "/";
+
+    return (
+        // wait lets the animation completely finish on exit of the prev component before the new component comes in 
+        <AnimatePresence mode="wait">
+            <motion.div
+                // it's going to wrap whichever page component the route is currently on - the key is necessary for AnimatePresence to know if a change is about to be made in the DOM to do the exit animation - since the children of this is a react router route attached to a page component, use the pathname to signal this change
+                key={location.pathname}
+                // bc of wait mode - need high opacity so it doesn't feel like a long blinking of nothing as the page transitions. need very low subtle y values as it'll be seen more easily as each does its own animation fully
+                // on enter - fade in from the top except landing
+                initial={isLanding ? { opacity: 0.9 } : { opacity: 0.8, y: -2 }}
+                // final destination
+                animate={{ opacity: 1, y: 0 }}
+                // on exit - fade out to the bottom except landing just fades out to avoid white space with the gradient bg
+                exit={isLanding ? { opacity: 0.7 } : { opacity: 0.8, y: 2 }}
+                // transition duration - easeOut slows at the end
+                transition={{ duration: .12, ease: easeOut}}
+            >
+                {/* this represents the component of the current route path */}
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    )
+}
